5ff5dbf7e942f2315be1e83540a4443a44ec6071
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2025-02-14 21:22:16 +0000

    update apps

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask,url_for, render_template,request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return render_template("index.html")
+
+@app.route('/hello')
+def hello():
+    return render_template("kontakt.html")
+
+@app.route('/noteikumi')
+def noteikumi():
+    return render_template("noteikumi.html")
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
